6705aa9f26e22c9f660ec9216e463fa14680ad5c
Umindu Chethiya <122140505+uminduchethiya@users.noreply.github.com>
2025-09-26 18:26:26 +0530

    url change

--- authapp/urls.py
+++ authapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login_view, name='login'),  # show login page first
+    path('home/', views.home, name='home'),    # optional: home page
     path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('customer-dashboard/', views.customer_dashboard, name='customer_dashboard'),
